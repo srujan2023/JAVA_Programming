a486f6ff6309671a65e32e988843e6367820c98c
Srujan A B <srujan2004ab@gmail.com>
2026-03-26 22:54:08 +0530

    Variables Program

---
+++ Variables.java
@@ -0,0 +1,9 @@
+public class Variables {
+    public static void main(String[] args) {
+char myvar1 = 'A';
+int myvar2 = 100;   
+System.out.println(myvar1);
+System.out.println(myvar2);     
+
+}
+}
