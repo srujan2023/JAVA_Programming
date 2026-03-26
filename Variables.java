bf41fee9aa97506e1202ebed3c5c90d6efd36cf4
Srujan A B <srujan2004ab@gmail.com>
2026-03-26 22:55:46 +0530

    Updated

--- Variables.java
+++ Variables.java
@@ -2,8 +2,9 @@ public class Variables {
     public static void main(String[] args) {
 char myvar1 = 'A';
 int myvar2 = 100;   
+boolean myvar3 = true;
 System.out.println(myvar1);
 System.out.println(myvar2);     
-
+System.out.println(myvar3);
 }
 }
