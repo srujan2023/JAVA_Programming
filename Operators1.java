807f1019ac8ee4a28dff4da37492a581a8d74674
Srujan2023 <Srujan2004ab@gmail.com>
2026-03-28 22:53:36 +0530

    Operators

---
+++ Operators1.java
@@ -0,0 +1,16 @@
+public class Operators1 {
+
+    public static void main(String[] args){
+
+        int a = 100;
+        int b = 200;
+
+        System.out.println("Addition of Two Numbers:"+(a+b));
+        System.out.println("Addition of Two Numbers:"+(a-b));
+        System.out.println("Addition of Two Numbers:"+(a*b));
+        System.out.println("Addition of Two Numbers:"+(a/b));
+        System.out.println("Addition of Two Numbers:"+(a%b));
+
+
+    }
+}
