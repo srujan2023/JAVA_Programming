e59b37258857716810a4609dda05e5befc9365e3
Srujan2023 <Srujan2004ab@gmail.com>
2026-03-28 22:41:31 +0530

    Variables Program

--- Variables.java
+++ Variables.java
@@ -1,10 +1,27 @@
 public class Variables {
     public static void main(String[] args) {
-char myvar1 = 'A';
-int myvar2 = 100;   
-boolean myvar3 = true;
-System.out.println(myvar1);
-System.out.println(myvar2);     
-System.out.println(myvar3);
+
+byte myfun = 100; //1 byte
+System.out.println(myfun);
+
+short myfun1 = 5000; //2 byte
+System.out.println(myfun1);
+
+int  myfun2 = 10000; //4 byte
+System.out.println(myfun2);
+
+long myfun3 = 100000000; //8 byte
+System.out.println(myfun3);
+
+float myfun4 = 1.40f;// 4 byte
+System.out.println(myfun4);
+
+double myfun5 = 100.9775437;// 8 byte
+System.out.println(myfun5);
+
+boolean myfun6 = true;
+System.out.println(myfun6);
+
+
 }
 }
