42cb9a5e58d3573ad7a0141963f9265d70b8ba4a
Srujan2023 <Srujan2004ab@gmail.com>
2026-03-28 22:58:35 +0530

    RElational Operators

---
+++ Operators2.java
@@ -0,0 +1,17 @@
+public class Operators2 {
+
+    public static void main(String[] args){
+
+        int a = 100;
+        int b = 200;
+
+        System.out.println(a==b);
+        System.out.println(a>=b);
+        System.out.println(a>b);
+        System.out.println(a<b);
+        System.out.println(a<=b);
+        System.out.println(a!=b);
+       
+
+    }
+}
