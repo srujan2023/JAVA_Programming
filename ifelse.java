744189b0659c98f79c47939a06577e909ff0e025
Srujan2023 <Srujan2004ab@gmail.com>
2026-03-29 21:53:49 +0530

    ifelse Program

---
+++ ifelse.java
@@ -0,0 +1,20 @@
+public class ifelse{
+
+    public static void main(String[] args){
+    
+    int a = 18;
+    int b = 18;
+    
+    if(a>b)
+    System.out.println("Not eligable to vote");
+    
+    else if(a==b)
+    System.out.println(" eligable to vote");
+    
+    else if(a<b)
+    System.out.println(" Not considered");
+    
+    else  
+    System.out.println("Reject the candidate-");
+    }
+    }
