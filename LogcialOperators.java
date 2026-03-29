889c29251639205a2770efd6ab7bf7c6f335c302
Srujan2023 <Srujan2004ab@gmail.com>
2026-03-29 21:31:15 +0530

    Logcial Operators

---
+++ LogcialOperators.java
@@ -0,0 +1,13 @@
+public class LogcialOperators{
+
+    public static void main(String[] args){
+
+        boolean x = false;
+        boolean y = true;
+
+    System.out.println(x&&y);
+    System.out.println(x||y);
+    System.out.println(!x);
+
+    }
+}
