168bdd137377cfb7c513a15ea0448a6e0e67a1fc
Srujan2023 <Srujan2004ab@gmail.com>
2026-03-29 21:40:55 +0530

    Increment  and  Decrement Program

---
+++ Increment_Decrement.java
@@ -0,0 +1,20 @@
+public class Increment_Decrement{
+
+    public static void main(String[] args){
+
+        int a = 100;
+        int b = 200;
+
+        a++;//Increment
+        b++;//Increment
+        System.out.println(a);
+        System.out.println(b);
+
+        
+        a--;//Decrement
+        b--;//Decrement
+        System.out.println(a);
+        System.out.println(b);
+
+    }
+}
