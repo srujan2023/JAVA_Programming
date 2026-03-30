945f8e8f9dc5cf555cbca293d6c3dc5e4f07e767
Srujan2023 <Srujan2004ab@gmail.com>
2026-03-30 22:02:11 +0530

    WhileLoop Program

---
+++ WhileLoop.java
@@ -0,0 +1,13 @@
+public class WhileLoop{
+
+    public static void main(String[] args){
+
+        //int i = 1;//For Odd Numbers Print
+        int i = 0;//For Even Numbers Print
+
+        while(i<=10){
+            System.out.println(i);
+           i=i+2;
+        }
+    }
+}
