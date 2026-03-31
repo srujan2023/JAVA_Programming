91e93ed6f5693e0aaedca4700470853178d2d571
Srujan2023 <Srujan2004ab@gmail.com>
2026-03-31 14:34:51 +0530

    Continue Program

---
+++ Continue.java
@@ -0,0 +1,12 @@
+public class Continue {
+
+    public static void main(String[] args){
+
+        for(int i =0; i<=10; i++){
+            if(i ==7 || i == 3 || i == 5){
+                continue;
+            }
+            System.out.println(i);
+    }
+    }
+}
