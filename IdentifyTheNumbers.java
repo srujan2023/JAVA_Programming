1ef90883738e10d51591b77f23a4eb4b1b4791d7
Srujan2023 <Srujan2004ab@gmail.com>
2026-03-31 14:41:38 +0530

    Identify The Numbers

---
+++ IdentifyTheNumbers.java
@@ -0,0 +1,21 @@
+public class IdentifyTheNumbers {
+
+    public static void main(String[] args){
+
+        int i = -20;
+
+        if(i > 0)
+        {
+            System.out.println("Postive Number");
+        }
+        else if(i < 0)
+        {
+            System.out.println("Negative Number");
+        }
+        else
+        {
+            System.out.println("Zero");
+        }
+
+    }
+}
