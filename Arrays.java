988aa9453b8c4db7fa0b54eb7776b922aab839b1
Srujan2023 <Srujan2004ab@gmail.com>
2026-04-01 22:54:06 +0530

    Arrays Program

---
+++ Arrays.java
@@ -0,0 +1,15 @@
+public class Arrays{
+
+    public static void main(String[] args){
+
+        int a[] = {100,200,300,400,500};
+
+        for(int i = 0; i<a.length-0;i++)
+        {
+            System.out.println(a[i]); //Print inside the Arrays Values
+
+        }
+            System.out.println(a.length);//Print the length of Arrays
+
+    }
+}
