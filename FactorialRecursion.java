c9bf76de3e3149818c48fd91977eba0dbff39948
Srujan2023 <Srujan2004ab@gmail.com>
2026-04-03 19:46:04 +0530

    FactorialRecursion Program

---
+++ FactorialRecursion.java
@@ -0,0 +1,15 @@
+public class FactorialRecursion {
+
+
+    static int factorial(int n) {
+        if (n == 0 || n == 1)
+            return 1;   
+        else
+            return n * factorial(n - 1); 
+    }
+
+    public static void main(String[] args) {
+        int num = 5;
+        System.out.println("Factorial = " + factorial(num));
+    }
+}
