6182e3781d2aad89d67891d9f246f6357fe06a4e
Srujan2023 <Srujan2004ab@gmail.com>
2026-04-04 22:18:30 +0530

    Palindrome Program

--- Palindrome.java
+++ Palindrome.java
@@ -6,3 +6,11 @@ public class Palindrome {
         for (int i = str.length() - 1; i >= 0; i--) {
             rev += str.charAt(i);
         }
+
+        if (str.equals(rev)) {
+            System.out.println("Palindrome");
+        } else {
+            System.out.println("Not Palindrome");
+        }
+    }
+}
